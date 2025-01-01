f22b781a0d96bc14e5a9054ad72f4f9c398b762f
Richard Cook <rcook@rcook.org>
2024-12-31 17:40:15 -0800

    Fix type annotation

--- dpreaderlib/utils.py
+++ dpreaderlib/utils.py
@@ -2,7 +2,7 @@ from typing import Any
 import itertools
 
 
-def indices(start: int = 0, count: int | None = None) -> Any:
+def indices(start: int = 0, count: int | None = None) -> itertools.count | range:
     if count is None:
         return itertools.count(start)
     else:
